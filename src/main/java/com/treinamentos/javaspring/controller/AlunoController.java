9d0aba685e8b2f8691db613352bbc8482e4fb6df
Edson Emanoel <edson.emanoelofc@gmail.com>
2024-06-16 23:44:34 -0300

    Rota de Listar Um também está funcionando

--- src/main/java/com/treinamentos/javaspring/controller/AlunoController.java
+++ src/main/java/com/treinamentos/javaspring/controller/AlunoController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.treinamentos.javaspring.model.Aluno;
 import com.treinamentos.javaspring.repostiory.AlunoRepository;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,14 +23,22 @@ public class AlunoController {
     @Autowired
     private AlunoRepository alunoRepository;
 
+    @CrossOrigin("*")
     @PostMapping("/cadastrar")
     public Aluno cadastrar(@RequestBody Aluno obj) {
         return alunoRepository.save(obj);
     }
 
+    @CrossOrigin("*")
     @GetMapping("/todos")
     public Iterable<Aluno> listar() {
         return alunoRepository.findAll();
     }
     
+    @CrossOrigin("*")
+    @GetMapping("/aluno/{id}")
+    public Optional<Aluno> listarUm(@PathVariable Long id) {
+        return alunoRepository.findById(id);
+    }
+
 }
