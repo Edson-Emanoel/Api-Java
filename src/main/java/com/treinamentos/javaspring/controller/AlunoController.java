ca16692e27013150e887abb77d8fbb5aa4159e06
Edson Emanoel <edson.emanoelofc@gmail.com>
2024-06-16 23:35:03 -0300

    Api funcionando apenas a rota de cadastrar

---
+++ src/main/java/com/treinamentos/javaspring/controller/AlunoController.java
@@ -0,0 +1,24 @@
+package com.treinamentos.javaspring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.treinamentos.javaspring.model.Aluno;
+import com.treinamentos.javaspring.repostiory.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/alunos")
+public class AlunoController {
+    
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @PostMapping("/cadastrar")
+    public Aluno cadastrar(@RequestBody Aluno obj) {
+        return alunoRepository.save(obj);
+    }
+    
+}
