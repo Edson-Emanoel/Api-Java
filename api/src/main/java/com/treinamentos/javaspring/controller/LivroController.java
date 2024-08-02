7d3c8beb0e30c1f96a59599950afc4f5df181e61
Edson-Emanoel <edson.emanoel2020@gmail.com>
2024-08-02 19:56:47 -0300

    Fiz o controller de Livro

--- api/src/main/java/com/treinamentos/javaspring/controller/LivroController.java
+++ api/src/main/java/com/treinamentos/javaspring/controller/LivroController.java
@@ -1,62 +1,61 @@
 package com.treinamentos.javaspring.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.treinamentos.javaspring.model.Aluno;
-import com.treinamentos.javaspring.repostiory.AlunoRepository;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.treinamentos.javaspring.model.Livro;
+import com.treinamentos.javaspring.repostiory.LivroRepository;
+
+@CrossOrigin("*")
 @RestController
-@RequestMapping("/api/alunos")
-public class AlunoController {
-    
+@RequestMapping("/api/livro")
+public class LivroController {
+
     @Autowired
-    private AlunoRepository alunoRepository;
+    private LivroRepository livroRepository;
 
     @CrossOrigin("*")
     @PostMapping("/cadastrar")
-    public Aluno cadastrar(@RequestBody Aluno obj) {
-        return alunoRepository.save(obj);
+    public Livro cadastrar(@RequestBody Livro obj) {
+        return livroRepository.save(obj);
     }
 
     @CrossOrigin("*")
     @GetMapping("/todos")
-    public Iterable<Aluno> listar() {
-        return alunoRepository.findAll();
+    public Iterable<Livro> listar() {
+        return livroRepository.findAll();
     }
-    
+
     @CrossOrigin("*")
-    @GetMapping("/aluno/{id}")
-    public Optional<Aluno> listarUm(@PathVariable Long id) {
-        return alunoRepository.findById(id);
+    @GetMapping("/{id}")
+    public Optional<Livro> listarUm(@PathVariable Long id) {
+        return livroRepository.findById(id);
     }
-    
+
     @CrossOrigin("*")
     @DeleteMapping("/remover/{id}")
     public ResponseEntity<Void> remover(@PathVariable Long id) {
-        alunoRepository.deleteById(id);
+        livroRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
     @CrossOrigin("*")
     @PutMapping("/alterar/{id}")
-    public Aluno atualizar(@RequestParam Aluno obj) {
+    public Livro atualizar(@RequestParam Livro obj) {
         if (obj.getId() != null) {
-            return alunoRepository.save(obj);    
+            return livroRepository.save(obj);    
         }   return obj;
     }
-
 }
